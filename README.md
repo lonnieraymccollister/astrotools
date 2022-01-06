f5d1b8ebff265bfd0b4b1dd8b35d2f5ef7dadd1d
Lonnie Ray McCollister <53101234+lonnieraymccollister@users.noreply.github.com>
2022-01-06 16:43:26 -0600

    update
    update

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ create csv img_pixel.csv
 
 examine image in csv and edit/change position if neccessary etc.
 
-step 2
+step (2 - 8bit_im_out)/(4 - 16bit)
 ((python astrotools.py) or (astrotools.exe(windows10/11))) 2
 create img_pixels2.csv from img_pixel.csv then enter the above command which then creates img_pixels2.png and shows img_pixels2.png
 
-step 3
+step (3 - 8bit_im)/(5 - 16bit)
 ((python astrotools.py) or (astrotools.exe(windows10/11))) 3 img_pixels2.png
 shows:  x - y - z(intensity in color and 3d)
 
--- astrotools.py
+++ astrotools.py
@@ -1,4 +1,4 @@
-import sys 
+import sys
 import os
 import csv
 import numpy as np
@@ -39,6 +39,35 @@ def plotto3d(img):
   # show it
   plt.show()
 
+def CSVcreateimage16():
+  my_data = genfromtxt('img_pixels2.csv', delimiter=',')
+  #Rescale to 0-255 and convert to uint8
+  #matplotlib.image.imsave('img_pixels2.png', my_data, cmap='gray')
+  #Rescale to 0-65535 and convert to uint16
+  rescaled = (65535.0 / my_data.max() * (my_data - my_data.min())).astype(np.uint16)
+  im = Image.fromarray(rescaled)
+  im.save('img_pixels2.png')
+  image_1 = imread('img_pixels2.png')
+  # plot raw pixel data
+  pyplot.imshow(image_1)
+  # show the figure
+  pyplot.show()
+
+def plotto3d16(img):
+  #lena = cv2.imread(img, 0)
+  lena = cv2.imread(img, cv2.IMREAD_GRAYSCALE | cv2.IMREAD_ANYDEPTH)
+  # downscaling has a "smoothing" effect
+  #lena = cv2.resize(lena, (121,121))  # create the x and y coordinate arrays (here we just use pixel indices)
+  xx, yy = np.mgrid[0:lena.shape[0], 0:lena.shape[1]]
+  # create the figure
+  fig = plt.figure()
+  ax = fig.add_subplot(projection='3d')
+  ax.plot_surface(xx, yy, lena ,rstride=1, cstride=1, cmap=plt.cm.jet, linewidth=0)
+  # show it
+  plt.show()
+
+
+
 if sys.argv[1] == '1':
     img = np.array(Image.open(sys.argv[2]))
     csvWriter("img_pixel", img)
@@ -48,3 +77,9 @@ if sys.argv[1] == '2':
 
 if sys.argv[1] == '3':
     plotto3d(sys.argv[2])
+
+if sys.argv[1] == '4':
+    CSVcreateimage16()
+
+if sys.argv[1] == '5':
+    plotto3d16(sys.argv[2])
