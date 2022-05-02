9d6b07aedbb0d3e703228f0f87f55bc81bc2d663
Lonnie Ray McCollister <53101234+lonnieraymccollister@users.noreply.github.com>
2022-05-02 06:14:02 -0500

    update
    update 16 bit flag

--- CLAHE.py
+++ CLAHE.py
@@ -4,14 +4,14 @@ import cv2
 import numpy as np
  
 # Reading the image from the present directory
-image = cv2.imread(sys.argv[1])
+image = cv2.imread((sys.argv[1]),-1)
 # Resizing the image for compatibility
 #image = cv2.resize(image, (485, 449))
  
 # The initial processing of the image
 # image = cv2.medianBlur(image, 3)
-image_bw = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#image_bw = image
+#image_bw = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image_bw = image
  
 # The declaration of CLAHE
 # clipLimit -> Threshold for contrast limiting
