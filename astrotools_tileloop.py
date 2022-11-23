6bd3d8af3712338b720d0c23fe9880fffe554f91
Lonnie Ray McCollister <53101234+lonnieraymccollister@users.noreply.github.com>
2022-11-23 06:58:09 -0600

    update
    update

--- astrotools_tileloop.py
+++ astrotools_tileloop.py
@@ -1,4 +1,4 @@
-#python astrotools_tileloop.py B.tif 950 950 700 1 4
+#python astrotools_tileloop.py Lm33t2.tif 950 950 700 1 4 H:\lonnielaptop\Astrotools\Cythonize\aL resultnew.tif
 from numba import jit
 import subprocess, shutil, os, sys
 import numpy as np
@@ -42,7 +42,13 @@ def main():
           imgcrop()
           PNGcreateimage16()
 
-@jit(nopython=False)
+@jit(nopython=True)
+def PNGcreateimage16loop(img, my_data, diameterp1):
+  for x in range(diameterp1):
+    for y in range(diameterp1):
+      my_data[x,y]=img[x,y]-min(img[x,y],img[x,diameter-y],img[diameter-x,y],img[diameter-x,diameter-y]) 
+  return my_data
+
 def PNGcreateimage16():
   global file, file1
   radius = 0  
@@ -56,9 +62,7 @@ def PNGcreateimage16():
   radius, radiusp1, diameter, diameterp1, one, two, three, four = imgcoord(radius, radiusp1, diameter, diameterp1, one, two, three, four)
   my_data = np.array(im1)
   img = my_data
-  for x in range(diameterp1):
-    for y in range(diameterp1):
-        my_data[x,y]=img[x,y]-min(img[x,y],img[x,diameter-y],img[diameter-x,y],img[diameter-x,diameter-y])     
+  my_data = PNGcreateimage16loop(img, my_data, diameterp1)    
 #Rescale to 0-65535 and convert to uint16
   my_data = my_data[my_data.any(axis=1)]
   my_data = np.transpose(my_data)
@@ -83,6 +87,7 @@ def imgcoord(radius, radiusp1, diameter, diameterp1, one, two, three, four):
   return radius, radiusp1, diameter, diameterp1, one, two, three, four
 
 def imgcrop():
+  global im
   global im1
   radius = 0
   radiusp1 = 0
@@ -95,5 +100,43 @@ def imgcrop():
   radius, radiusp1, diameter, diameterp1, one, two, three, four = imgcoord(radius, radiusp1, diameter, diameterp1, one, two, three, four)
   im1 = im.crop((one, two, three, four)) 
 
+def auto_stack_16bit():
+  # stack images 
+  file_list = os.listdir(sys.argv[7])
+  file_list = [os.path.join(sys.argv[7], x)
+                   for x in file_list if x.endswith(('.png'))]
+  M = np.eye(3, 3, dtype=np.float32)
+  first_image = None
+  stacked_image = None
+  for file in file_list:
+      image = cv2.imread(file, flags=cv2.IMREAD_ANYDEPTH).astype(np.float32) / 65535
+      print(file)
+      if first_image is None:
+          first_image = image
+          stacked_image = image
+      else:
+          stacked_image += image
+  stacked_image /= len(file_list)   
+  stacked_image = (stacked_image*65535).astype(np.uint16)
+  file_list = [os.path.join(sys.argv[8], x)
+                   for x in file_list if x.endswith(('.jpg', '.png','.bmp'))]
+  cv2.imwrite(sys.argv[8], stacked_image)
+  print("===>Stacking complete<===")
+  print("===>Stacking complete<===")
+  print("===>Stacking complete<===")
+
+def CLAHE():
+  image = cv2.imread((sys.argv[8]),-1) 
+  image_bw = image 
+  # The declaration of CLAHE
+  # clipLimit -> Threshold for contrast limiting
+  #clahe = cv2.createCLAHE(clipLimit = 5)
+  #final_img = clahe.apply(image_bw) + 30
+  clahe = cv2.createCLAHE(clipLimit = 2)
+  final_img = clahe.apply(image_bw)
+  cv2.imwrite(('final_img'+sys.argv[1]),final_img)
+
 if __name__ == "__main__":
   main()
+  auto_stack_16bit()
+  CLAHE()
