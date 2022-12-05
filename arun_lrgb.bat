0b1997a84452fa59c09be94edb744444dd94bfc6
Lonnie Ray McCollister <53101234+lonnieraymccollister@users.noreply.github.com>
2022-12-04 19:26:44 -0600

    update
    update

--- arun_lrgb.bat
+++ arun_lrgb.bat
@@ -1,5 +1,9 @@
-call astrotools_tileloop.exe Lbarnard33c2.tif 950 950 700 0 99 
-call astrotools_tileloop.exe Rbarnard33c2.tif 950 950 700 0 99 
-call astrotools_tileloop.exe Gbarnard33c2.tif 950 950 700 0 99 
-call astrotools_tileloop.exe Bbarnard33c2.tif 950 950 700 0 99 
+start /wait astrotools_tileloop.exe Lngc7318t4.tif 950 950 700 0 99 
+echo %errorlevel%
+start /wait astrotools_tileloop.exe Rngc7318t4.tif 950 950 700 0 99 
+echo %errorlevel%
+start /wait astrotools_tileloop.exe Gngc7318t4.tif 950 950 700 0 99 
+echo %errorlevel%
+start /wait astrotools_tileloop.exe Bngc7318t4.tif 950 950 700 0 99
+echo %errorlevel% 
 
